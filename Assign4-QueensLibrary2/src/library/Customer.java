2781d45442dbb450b7175c794576bb6c1eb58e9b
Carson Cook <cook.carson38@gmail.com>
2017-03-25 22:30:41 -0400

    created customer class

---
+++ Assign4-QueensLibrary2/src/library/Customer.java
@@ -0,0 +1,58 @@
+package library;
+
+import library.Enums.CustomerType;
+
+/**
+ * Created by Carson on 25/03/2017.
+ * 14cdwd
+ * Class defining information about a customer.
+ */
+public class Customer {
+    private CustomerType type;
+    private String firstName, lastName, department;
+    private int id;
+
+    public Customer(CustomerType type, String firstName, String lastName, String department, int id) {
+        setType(type);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setDepartment(department);
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public CustomerType getType() {
+        return type;
+    }
+
+    public void setType(CustomerType type) {
+        this.type = type;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ Assign4-QueensLibrary2/src/library/Enums/CustomerType.java
@@ -0,0 +1,8 @@
+package library.Enums;
+
+/**
+ * Created by Carson on 25/03/2017.
+ */
+public enum CustomerType {
+    STUDENT, EMPLOYEE
+}
